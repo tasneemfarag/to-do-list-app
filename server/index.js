dd382393c40fa3b31c89035834e811a673e392e6
Tasneem Farag <tasneem.farag@ibm.com>
2019-02-04 16:08:00 -0500

    WIP

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const Todo = mongoose.model('Todo', {
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos: [Todo]
   }
   type Todo {
     id: ID!
@@ -18,22 +19,33 @@ const typeDefs = `
     complete: Boolean!
   }
   type Mutation {
-    createTodo(text: String!) Todo
+    createTodo(text: String!): Todo
+    updateTodo(id: ID!, complete: Boolean!): Boolean
+    removeTodo(id: ID!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    todos: () => Todo.find()
   },
   Mutation: {
     createTodo: async (_, { text }) => {
         const todo = new Todo({ text, complete: false });
         await todo.save();
         return todo;
-    }
+    },
+    updateTodo: async (_, {id, complete}) => {
+      await Todo.findByIdAndUpdate(id, {complete});
+      return true;
+    },
+    removeTodo: async (_, { id }) => {
+        await Todo.findByIdAndRemove(id);
+        return true;
+    },    
   }
-}
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 mongoose.connection.once("open", function() {
