b6b13860c641c7f15711beec3e107d861f8ef078
Tasneem Farag <tasneem.farag@ibm.com>
2019-02-04 16:24:48 -0500

    Adding client folder+

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import gql from "graphql-tag";
 import { graphql } from 'react-apollo';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
 
 const TodosQuery = gql`
 {
@@ -14,6 +21,22 @@ const TodosQuery = gql`
 `;
 
 class App extends Component {
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
   render() {
     const { 
       data: { loading, todos }
@@ -26,6 +49,23 @@ class App extends Component {
         <div style={{ margin: 'auto', width: 400 }}>
           <Paper elevation={1}>
           {todos.map(todo => <div key={`${todo.id}-todo-item`}>{todo.text}</div>)}
+          <List>
+        {[0, 1, 2, 3].map(value => (
+          <ListItem key={value} role={undefined} dense button onClick={this.handleToggle(value)}>
+            <Checkbox
+              checked={this.state.checked.indexOf(value) !== -1}
+              tabIndex={-1}
+              disableRipple
+            />
+            <ListItemText primary={`Line item ${value + 1}`} />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Comments">
+                <CommentIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
           </Paper>
         </div>
       </div>
@@ -34,4 +74,26 @@ class App extends Component {
 }
 
 export default graphql(TodosQuery)(App);
- 
+
+
+class CheckboxList extends React.Component {
+  state = {
+    checked: [0],
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
